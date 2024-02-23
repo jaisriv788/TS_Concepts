ad68d8687f8b9566ee48b2ad749c7811283e2d6f
Jai Srivastava <131600482+jaisriv788@users.noreply.github.com>
2024-02-23 21:38:10 +0530

    Update 14.zod_implementation.ts

--- 14.zod_implementation.ts
+++ 14.zod_implementation.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import { z } from "zod";
 import express from "express";
 
 const app = express();
@@ -7,21 +7,26 @@ const app = express();
 const userProfileSchema = z.object({
   name: z.string().min(1, { message: "Name cannot be empty" }),
   email: z.string().email({ message: "Invalid email format" }),
-  age: z.number().min(18, { message: "You must be at least 18 years old" }).optional(),
+  age: z
+    .number()
+    .min(18, { message: "You must be at least 18 years old" })
+    .optional(),
 });
+
+type typeDeclaration = z.infer<typeof userProfileSchema>;
 
 app.put("/user", (req, res) => {
   const { success } = userProfileSchema.safeParse(req.body);
-  const updateBody = req.body; // how to assign a type to updateBody?
+  const updateBody: typeDeclaration = req.body; // how to assign a type to updateBody?
 
   if (!success) {
     res.status(411).json({});
-    return
+    return;
   }
   // update database here
   res.json({
-    message: "User updated"
-  })
+    message: "User updated",
+  });
 });
 
 app.listen(3000);
